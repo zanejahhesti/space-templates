6e6b501587480e544363be8328943b54668cb55b
jnsougata <jnsougata@gmail.com>
2023-02-13 00:52:29 +0530

    Update main.go & new README.md

--- go-app/main.go
+++ go-app/main.go
@@ -1,22 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/deta/deta-go/deta"
+	"github.com/deta/deta-go/service/base"
+	"github.com/deta/deta-go/service/drive"
+	"github.com/labstack/echo/v4"
 )
 
-func main() {
-	http.HandleFunc("/", root)  // Register the root handler
-	log.Println("Running at: http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))  // Start the server
-}
+var d, _ = deta.New()
 
-func root(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello World!"))
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	}
+func main() {
+	e := echo.New()
+
+	e.GET("/", func(ctx echo.Context) error {
+		return ctx.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.GET("/users", func(ctx echo.Context) error {
+		db, err := base.New(d, "users")
+		if err != nil {
+			return ctx.String(http.StatusInternalServerError, "Internal Server Error")
+		}
+		var users []map[string]interface{}
+		_, err = db.Fetch(&base.FetchInput{Dest: &users})
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			return ctx.String(http.StatusInternalServerError, "Internal Server Error")
+		}
+		return ctx.JSON(http.StatusOK, users)
+	})
+
+	e.GET("/avatars/:id", func(ctx echo.Context) error {
+		id := ctx.Param("id")
+		drive, _ := drive.New(d, "avatars")
+		r, err := drive.Get(fmt.Sprintf("%s.png", id))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			return ctx.String(http.StatusInternalServerError, "Internal Server Error")
+		}
+		defer r.Close()
+		return ctx.Stream(http.StatusOK, "image/png", r)
+	})
+	
+	fmt.Println("Server running at: http://localhost:8080")
+	log.Fatal(e.Start(":8080"))
 }
