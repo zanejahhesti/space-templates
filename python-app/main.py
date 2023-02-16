58e9f771d655c96d0b8e2e1a484573595460fce1
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-02-16 18:18:24 -0500

    Refactor main.py

--- python-app/main.py
+++ python-app/main.py
@@ -1,37 +1,46 @@
 from deta import Deta
-from fastapi import FastAPI
-from fastapi import Request
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse, Response
 
-
 app = FastAPI()
-db = Deta().Base("users")
-drive = Deta().Drive("avatars")
+deta = Deta()
+users_db = deta.Base("users")
+avatars_drive = deta.Drive("avatars")
 
 
 @app.get("/")
-def root():
-    return JSONResponse({"message": "Hello World"})
+async def root():
+    return JSONResponse({"message": "Hello World!"})
 
-@app.post("/users")
-def create_user(user: dict):
-    try:
-        resp = db.insert(user, user["id"])
-        resp.status = 201
-    except Exception as e:
-        resp = {"message": str(e)}
-        resp.status = 409
-    return JSONResponse(resp, status_code=resp.status)
 
 @app.get("/users/{user_id}")
-def get_user(user_id: str):
-    return db.get(user_id)
+async def get_user(user_id: str):
+    user = users_db.get(user_id)
+    if user is None:
+        raise HTTPException(status_code=404)
+    return user
 
-@app.post("/users/{user_id}/avatar")
-async def upload_avatar(req: Request, user_id: str):
-    file = await req.body()
-    return drive.put(f'{user_id}.png', file)
+
+@app.post("/users")
+async def create_user(user: dict):
+    try:
+        response = users_db.insert(user, user["id"])
+        status = 201
+    except Exception as exc:
+        response = {"message": str(exc)}
+        status = 409
+    return JSONResponse(response, status_code=status)
+
 
 @app.get("/users/{user_id}/avatar")
-def get_avatar(user_id: str):
-    return Response(drive.get(f'{user_id}.png'), media_type="image/png")
+async def get_avatar(user_id: str):
+    avatar = avatars_drive.get(f"{user_id}.png")
+    if avatar is None:
+        raise HTTPException(status_code=404)
+    return Response(avatar, media_type="image/png")
+
+
+@app.post("/users/{user_id}/avatar")
+async def upload_avatar(request: Request, user_id: str):
+    file = await request.body()
+    return avatars_drive.put(f"{user_id}.png", file)
