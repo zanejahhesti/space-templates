f47d38e41f4a48ea722aae03110581bad5a45148
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-02-16 23:06:39 -0500

    Update and format main.ts

--- deno-app/main.ts
+++ deno-app/main.ts
@@ -1,22 +1,18 @@
 import { serve } from "https://deno.land/std@0.173.0/http/server.ts";
 
-const port = 8080;
-
 const handler = (request: Request): Response => {
   if (request.method === "GET") {
     const body = new TextDecoder().decode(Deno.readFileSync("./static/index.html"));
     return new Response(body, {
       status: 200,
-      headers: { "content-type": "text/html" },
+      headers: { "Content-Type": "text/html" },
     });
   }
-  const naResp = new TextDecoder().decode(Deno.readFileSync("./static/405.html"));
-  return new Response(
-    naResp, {
-      status: 405,
-      headers: { "content-type": "text/html" },
-    }
-  );
+  return new Response("Method Not Allowed", {
+    status: 405,
+  });
 };
 
-await serve(handler, { port });
+await serve(handler, {
+  port: +Deno.env.get("PORT")! || 8080,
+});
