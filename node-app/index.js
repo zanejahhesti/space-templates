cc3c0d39913e4f38be611c11e6c06acf84c8b70d
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-02-18 22:57:48 -0500

    Create example todo app

--- node-app/index.js
+++ node-app/index.js
@@ -1,12 +1,36 @@
-const express = require('express')
-const app = express()
+import fs from "fs";
+import express from "express";
+import { Base } from "deta";
 
-const port = process.env.PORT
+const app = express();
+const port = parseInt(process.env.PORT) || 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello from Space!')
-})
+app.use(express.json());
+
+app.get("/", async (request, response) => {
+  response.send(fs.readFileSync("./static/index.html", "utf8"));
+});
+
+app.get("/todos", async (request, response) => {
+  // Connect to a Base for storing todo items.
+  const todos_base = Base("todos");
+  // Fetch all items from the Base.
+  const todos = await todos_base.fetch();
+  // Return the items as JSON.
+  response.send(todos.items);
+});
+
+app.post("/todos", async (request, response) => {
+  // Connect to a Base for storing todo items.
+  const todos_base = Base("todos");
+  // Get the item from the request body.
+  const item = request.body;
+  // Put the item into the Base.
+  const resp = await todos_base.put(item);
+  // Return the response as JSON.
+  response.send(resp);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server listening on port ${port}`);
+});
